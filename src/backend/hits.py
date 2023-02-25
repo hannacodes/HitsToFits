07958f8cac03a6292e44400b9b82e28a635ef99f
Hanna Koh <hjk5643@rit.edu>
2023-02-25 13:35:18 -0500

    you can access specific components of data now :)

--- src/backend/hits.py
+++ src/backend/hits.py
@@ -1,5 +1,7 @@
 import spotipy
 import os
+import math
+import json
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
 # this class gets the data from the song
@@ -7,16 +9,38 @@ load_dotenv()
 CID = os.getenv('CLIENTID')
 CIS = os.getenv('CLIENTSECRET')
 
-
 #Authentication - without user
 client_credentials_manager = SpotifyClientCredentials(client_id=CID, client_secret=CIS)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-print("enter a song: ")
-track_uri = input()
+def getAllData(track_uri):
+    return sp.audio_features(track_uri)[0]
 
+# this gets the mode of the song
+def getMode(features):
+    return features["mode"]
 
-def getAllData():
-    print(sp.audio_features(track_uri)[0])
+def convertLoudness(loud): 
+    power = 10**(loud/20)
+    return power
 
-getAllData()
+# this gets the loudness of a song :)
+def getLoudness(features):
+    # normalize loudness
+    loud = features["loudness"]
+    return convertLoudness(loud)
+
+def getEnergy(features):
+    return features["energy"]
+
+def getValence(features):
+    return features["valence"]
+
+if __name__ == '__main__':
+    print("enter a song: ")
+    track_uri = input()
+    features = getAllData(track_uri)
+    print(getMode(features))
+    print(getLoudness(features))
+    print(getEnergy(features))
+    print(getValence(features))
