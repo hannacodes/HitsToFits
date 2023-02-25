c1c516bf4ff90811230f3c4f296e44bcebfed750
Hanna Koh <hjk5643@rit.edu>
2023-02-25 17:37:56 -0500

    added example closet images

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
 import sys
 import os
  
@@ -13,6 +14,12 @@ app = Flask(__name__)
 
 nav = "<a href='/'>home</a> <a href='/about/'>about</a>"
 
+song_url = ""
+ALLOWED_EXTENSIONS=[".png", ".jpg"]
+UPLOAD_FOLDER = './static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 @app.route('/')
 def input():
     return render_template('input.html')
@@ -24,22 +31,35 @@ def song():
         return f"the url is invalid"
     if request.method == 'POST':
         form_data = request.form
-        features = hits.getAllData(form_data["song"])
+        song_url = form_data["song"]
+        features = hits.getAllData(song_url)
         mode = hits.getMode(features)
-        return "<h1> Mode: "+str(mode)+"</h1>"
-    
+        return redirect("/closet")
+        
 @app.route('/closet')
 def closet(): 
     return render_template("closet.html")
+'''
+def allowedFile(filename): 
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+'''
 
 @app.route('/upload', methods=["GET", "POST"])
 def upload(): 
     if request.method == 'POST': 
         if request.form.get("existing") == "ViewExisting":
-            return "existing"
+            return redirect("/existing")
         else: 
-            print(request.form)
-            photolst = request.form.getlist("photos")
+            print(request.files)
+            photolst = request.files.getlist("file")
+            for file in photolst: 
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return "you uploaded: " + str(photolst)
     if request.method == 'GET': 
         return f"the url is invalid"
+
+@app.route("/precloset")
+def precloset():
+    return render_template("precloset.html")
