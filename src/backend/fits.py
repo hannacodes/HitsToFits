fe4510679d5414b1a295aacc3c88269d44a455c8
Ryan <rto9185@rit.edu>
2023-02-25 18:48:59 -0500

    google vision api working poggers

---
+++ src/backend/fits.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+# from __future__ import print_function
+import google
+from google.cloud import vision
+from googleapiclient.discovery import build
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ='hit2fitKey.json'
+
+image_uri = 'gs://cloud-samples-data/vision/using_curl/shanghai.jpeg'
+
+client = vision.ImageAnnotatorClient()
+image = vision.Image() # Py2+3 if hasattr(vision, 'Image') else vision.types.Image()
+image.source.image_uri = image_uri
+
+response = client.label_detection(image=image)
+
+print('Labels (and confidence score):')
+print('=' * 30)
+for label in response.label_annotations:
+    print(label.description, '(%.2f%%)' % (label.score*100.))
