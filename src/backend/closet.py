bb995489f9ab79cc1c9c42caff8489d692f72150
Ryan <rto9185@rit.edu>
2023-02-25 22:31:52 -0500

    access bucket

--- src/backend/closet.py
+++ src/backend/closet.py
@@ -2,26 +2,24 @@ import os
 from google.cloud import storage
 
 # authenticate google
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ='bucket2.json'
-
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ='bucketKey.json'
 storage_client = storage.Client()
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    # bucket_name = "your-bucket-name"
+    
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name)
 
-bucket_name = 'bucket'
-blobs = storage_client.list_blobs(bucket_name)
-for blob in blobs:
-    print(blob.name)
+    # Note: The call returns a response only when the iterator is consumed.
+    blobList = []
+    for blob in blobs:
+        blobList.append(blob.name)
+    
+    return blobList
 
 # # accessing a bucket
 # closet = storage.get_bucket('bucket')
 
-# # upload to bucket (closet)
-# def uploadToBucket(blobName, filePath, bucketName):
-#     try:
-#         bucket = storage_client.get_bucket(bucketName)
-#         blob = bucket.blob(blobName)
-#         blob.upload_from_file_name(filePath)
-#         return True
-#     except Exception as e:
-#         print(e)
-#         return False
-    
+# upload to bucket
+# def uploadToBucket(fileName):
--- src/backend/fits.py
+++ src/backend/fits.py
@@ -4,42 +4,38 @@ import google
 from google.cloud import vision
 from googleapiclient.discovery import build
 import os
+from closet import list_blobs
 
 # authenticate google
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ='hit2fitKey.json'
 
-image_uri = 'gs://hit2fit/blue.png'
 
-client = vision.ImageAnnotatorClient()
-image = vision.Image() # Py2+3 if hasattr(vision, 'Image') else vision.types.Image()
-image.source.image_uri = image_uri
+def getDominantColor(blobName):
+    image_uri = 'gs://hit2fit/' + blobName
 
-response = client.label_detection(image=image)
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image() # Py2+3 if hasattr(vision, 'Image') else vision.types.Image()
+    image.source.image_uri = image_uri
 
-def getLabels():
-    print('Labels (and confidence score):')
-    print('=' * 30)
-    for label in response.label_annotations:
-        print(label.description, '(%.2f%%)' % (label.score*100.))
-
-# getLabels()
-
-def getDominantColor():
     response = client.image_properties(image=image)
     props = response.image_properties_annotation
     
-    print("\nProperties:")
     domColor = props.dominant_colors.colors[0]
     colorString = domColor.color
-    # color = json.loads(colorString)
-    # print(color["red"], color["green"], color["blue"])
 
     if response.error.message:
         raise Exception(
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
+
     return [int(colorString.red), int(colorString.green), int(colorString.blue)]
 
-
-print(getDominantColor())
+def getBucketColors(bucketName):
+    bucketNames = list_blobs(bucketName)
+    
+    retList = []
+    for blobName in bucketNames:
+        retList.append(getDominantColor(blobName))
+    
+    return retList
