ccd0de711ea6d994aa257fefd8d949b4b134174d
Hanna Koh <hjk5643@rit.edu>
2023-02-26 00:54:15 -0500

    you can upload things to the cloud bucket!!

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, g, session
 from werkzeug.utils import secure_filename
+import requests
+import xmltodict as XD
 import sys
 import os
 
@@ -8,7 +10,8 @@ path = os.path.join(os.path.dirname(__file__), os.pardir)
 sys.path.append(path)
 sys.path.append('../backend')
 
-from backend import hits, fits, calculateFeatures
+from backend import hits, fits, calculateFeatures, closet
+from closet import uploadBlob, listBlobs
 
 app = Flask(__name__)
 
@@ -16,20 +19,20 @@ nav = "<a href='/'>home</a> <a href='/about/'>about</a>"
 
 song_url = ""
 features = {}
-ALLOWED_EXTENSIONS=[".png", ".jpg"]
+ALLOWED_EXTENSIONS = [".png", ".jpg"]
 UPLOAD_FOLDER = './static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-    
 
 @app.route('/')
 def input():
     return render_template('input.html')
 
-@app.route('/song', methods = ['POST', 'GET'])
+
+@app.route('/song', methods=['POST', 'GET'])
 def song():
-    print(request.method )
-    if request.method == 'GET': 
+    print(request.method)
+    if request.method == 'GET':
         return f"the url is invalid"
     if request.method == 'POST':
         form_data = request.form
@@ -41,41 +44,51 @@ def song():
         f.close()
         mode = hits.getMode(features)
         return redirect("/closet")
-        
+
+
 @app.route('/closet')
-def closet(): 
+def closet():
     return render_template("closet.html")
+
+
 '''
-def allowedFile(filename): 
+def allowedFile(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 '''
 
+
 @app.route('/upload', methods=["GET", "POST"])
-def upload(): 
-    if request.method == 'POST': 
+def upload():
+    if request.method == 'POST':
         if request.form.get("existing") == "View Existing":
             return redirect("/existing")
-        else: 
+        else:
             print(request.files)
             photolst = request.files.getlist("file")
-            for file in photolst: 
+            for file in photolst:
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                fullpath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(fullpath)
+                uploadBlob("hit2fit", fullpath, file.filename)
+            photos = listBlobs("hit2fit")
+            photos = ['https://storage.googleapis.com/hit2fit/' + file for file in photos]
 
-            photos = os.listdir('./static/uploads/')
-            print(photos)
-            photos.remove("exampleCloset")
-            photos = ['uploads/' + file for file in photos]
+            # photos = os.listdir('./static/uploads/')
+            # print(photos)
+            # photos.remove("exampleCloset")
+            # photos = ['uploads/' + file for file in photos]
             return render_template("precloset.html", photos=photos)
-        
-    if request.method == 'GET': 
+
+    if request.method == 'GET':
         return f"the url is invalid"
 
+
 @app.route("/existing")
-def precloset():
-    photos = os.listdir('./static/uploads/exampleCloset/')
-    photos = ['uploads/exampleCloset/' + file for file in photos]
+def precloset(): 
+    photos = listBlobs("hit2fit")
+    photos = ['https://storage.googleapis.com/hit2fit/' + file for file in photos]
+    print(photos)
     return render_template("precloset.html", photos=photos)
 
 @app.route("/matching")
